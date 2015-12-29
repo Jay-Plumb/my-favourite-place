df92ecd09445c604bf5f05c0c59c7c86f43f5341
Jay-Plumb <jaymjplumb@gmail.com>
2015-12-29 15:44:00 +0000

    Initial Commit

---
+++ my-favourite-place/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  my-favourite-place
+//
+//  Created by Jason Plumb on 2015. 12. 29..
+//  Copyright © 2015년 JasonPlumb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
