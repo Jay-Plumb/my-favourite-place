f436b1fdd2530c1780808be2c6e448c092a0a3a7
Jay-Plumb <jaymjplumb@gmail.com>
2015-12-29 16:22:13 +0000

    first commit

---
+++ my-favourite-place/RoundButton.swift
@@ -0,0 +1,21 @@
+//
+//  roundButton.swift
+//  my-favourite-place
+//
+//  Created by Jason Plumb on 2015. 12. 29..
+//  Copyright © 2015년 JasonPlumb. All rights reserved.
+//
+
+import UIKit
+
+class RoundButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
